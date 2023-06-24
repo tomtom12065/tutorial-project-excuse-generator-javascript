42176d1fb67e8534744e5e5ecc9449ff16a36f6f
tomtom12065 <100985000+tomtom12065@users.noreply.github.com>
2023-06-24 22:07:24 +0000

    first commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
 let action = ['ate', 'peed', 'crushed', 'broke'];
 let what = ['my homework', 'the keys', 'the car'];
 let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
-excuse.innerHTML = generator(who,action,what,when)
+ excuse.innerHTML = generator(who,action,what,when)
 
 }
 let generator=(who, action,what,when)=>{
